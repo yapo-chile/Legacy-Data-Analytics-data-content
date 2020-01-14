bdac27fa1e155941e0a9662fed8f6a01df81c8b1
Luis Barrera <luis.barrera@schibsted.com>
2020-01-14 17:04:09 -0300

    [feat/seller-return-over-current]

--- seller-return-over-current/app/main.py
+++ seller-return-over-current/app/main.py
@@ -2,11 +2,37 @@
 # utf-8
 import sys
 import logging
+import pandas as pd
 from infraestructure.conf import getConf
 from infraestructure.psql import Database
 from utils.query import Query
 from utils.read_params import ReadParams
 from utils.time_execution import TimeExecution
+
+def source_data(params: ReadParams,
+                config: getConf):
+    query = Query()
+    DB_SOURCE = Database(conf=config.db)
+    data_current = DB_SOURCE.select_to_dict(query \
+                                           .seller_return_over_current(params))
+    data_past = DB_SOURCE.select_to_dict(query \
+                                        .seller_return_over_past(params))
+    DB_SOURCE.close_connection()
+    return data_past, data_current
+
+
+
+def destiny_data(params: ReadParams,
+                 config: getConf,
+                 data_past: pd.DataFrame,
+                 data_curent: pd.DataFrame) -> None:
+    query = Query()
+    DB_WRITE = Database(conf=config.db)
+    DB_WRITE.execute_command(query.delete_past(params))
+    DB_WRITE.execute_command(query.delete_current(params))
+    DB_WRITE.insert_current(data_curent)
+    DB_WRITE.insert_past(data_past)
+    DB_WRITE.close_connection()
 
 
 if __name__ == '__main__':
@@ -18,15 +44,7 @@ if __name__ == '__main__':
     LOG_FORMAT = DATE_FORMAT + INFO_FORMAT
     logging.basicConfig(format=LOG_FORMAT, level=logging.INFO)
     PARAMS = ReadParams(sys.argv)
-    QUERY = Query()
-    DB_WRITE = Database(conf=CONFIG.db)
-    DB_WRITE.execute_command(QUERY.delete_data(PARAMS))
-    DATA_CURRENT = DB_WRITE.select_to_dict(QUERY \
-                                           .seller_return_over_current(PARAMS))
-    DATA_PAST = DB_WRITE.select_to_dict(QUERY \
-                                        .seller_return_over_past(PARAMS))
-    DB_WRITE.insert_current(DATA_CURRENT)
-    DB_WRITE.insert_past(DATA_PAST)
-    DB_WRITE.close_connection()
+    DATA_PAST, DATA_CURRENT = source_data(PARAMS, CONFIG)
+    destiny_data(PARAMS, CONFIG, DATA_PAST, DATA_CURRENT)
     TIME.get_time()
     LOGGER.info('Process ended successfully.')
