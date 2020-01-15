98751538498325d493f16848fc1ca37b38383615
Luis Barrera <luis.barrera@schibsted.com>
2020-01-15 11:02:26 -0300

    [feat/seller-return-over-current]: Generic compile images for each module.

---
+++ scripts/install-dependencies.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+
+MODULE_COMPILE=""
+
+function GET_BUILD_MODULE(){
+    GIT_CURRENT_BRANCH=$(git rev-parse --abbrev-ref HEAD)
+    GIT_LAST_COMMIT=$(git log -p --name-only --oneline | head -1 | awk '{print $1}')
+    GIT_LAST_MERGE=$(git log -p --name-only --oneline | grep "Merge" | head -1 | awk '{print $1}')
+    echo "GIT_CURRENT_BRANCH: ${GIT_CURRENT_BRANCH}"
+    echo "GIT_LAST_COMMIT: ${GIT_LAST_COMMIT}"
+    echo "GIT_LAST_MERGE: ${GIT_LAST_MERGE}"
+    MODULE_COMPILE=$(git log -p --name-only --oneline ${GIT_LAST_MERGE}..${GIT_LAST_COMMIT} | grep "/" | grep  -v " " | grep -v ".md" | awk '{split($0, val, "/"); print val[1]}' | sort | uniq -c | awk '{print $2}')
+}
+
+function INSTALL_DEPENDENCIES(){
+    IFS=' '
+    read -ra ADDR <<< "${MODULE_COMPILE}"
+    for MODULE in "${ADDR[@]}"; do
+        echo "make -C ${MODULE} install"
+        make -C ${MODULE} install
+    done
+}
+
+GET_BUILD_MODULE
+INSTALL_DEPENDENCIES
+
