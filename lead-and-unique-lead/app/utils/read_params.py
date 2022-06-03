0f497b0704e7d01aa4bef3f88ca2e974d29072c8
andres <andres.vivallo@yapo.cl>
2022-06-03 15:35:04 -0400

    edit timedelta in read params in lead_and_unique_lead and peak_content_metrics

--- lead-and-unique-lead/app/utils/read_params.py
+++ lead-and-unique-lead/app/utils/read_params.py
@@ -123,10 +123,10 @@ class ReadParams:
         self.logger.info('Validate params.')
         current_date = datetime.datetime.now()
         if self.date_from is None:
-            temp_date = current_date + timedelta(days=-1)
+            temp_date = current_date + timedelta(days=-2)
             self.date_from = temp_date
         if self.date_to is None:
-            temp_date = current_date + timedelta(days=-1)
+            temp_date = current_date + timedelta(days=-2)
             self.date_to = temp_date
         if self.master is None:
             self.master = 'local'
